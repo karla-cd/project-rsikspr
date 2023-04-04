555efed2569951e22f451a6a875c1e9f88e92036
karla_cd <karla.cosic-dragan@fer.hr>
2023-04-04 13:27:41 +0200

    Second commit

--- src/main/java/fer/rsikspr/project/Controller.java
+++ src/main/java/fer/rsikspr/project/Controller.java
@@ -1,6 +1,6 @@
 package fer.rsikspr.project;
 
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    // http://localhost:8080/health
+
     @GetMapping("/health")
     public ResponseEntity<String> getOk() {
-        return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("OK");
     }
 
 }
